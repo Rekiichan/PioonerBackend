010532b36b000fb5c870233dca22570a5074607f
Rekiichan <dtnhan.ete.dut@gmail.com>
2022-12-04 01:43:47 +0700

    add event model

---
+++ Model/Event.cs
@@ -0,0 +1,18 @@
+﻿
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
+
+namespace Pioneer_Backend.Model
+{
+    public class Event
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string EventName { get; set; }
+        public DateTime EventDate { get; set; }
+        [ValidateNever]
+        public string ImageUrl { get; set; }
+        public string Description { get; set; }
+    }
+}
