456e7a02863e696af1e885000a4572e6b8040391
Rekiichan <dtnhan.ete.dut@gmail.com>
2022-12-06 11:39:04 +0700

    add config for log debug

--- Program.cs
+++ Program.cs
@@ -1,11 +1,20 @@
+using Serilog;
 using Microsoft.EntityFrameworkCore;
 using Pioneer_Backend.DataAccess.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// TODO: Uncomment if u want to see log in txt
 
-builder.Services.AddControllers();
+//Log.Logger = new LoggerConfiguration().MinimumLevel.Debug()
+//    .WriteTo.File("Log/MemberLog.txt", rollingInterval: RollingInterval.Day).CreateLogger();
+//builder.Host.UseSerilog();
+
+
+builder.Services.AddControllers(option =>
+{
+    option.ReturnHttpNotAcceptable = true;
+}).AddNewtonsoftJson().AddXmlDataContractSerializerFormatters();
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
     builder.Configuration.GetConnectionString("DefaultConnection"))
 );
