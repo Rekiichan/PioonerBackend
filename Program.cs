db70b60d61d5bbc5559761363bd1a663a693d408
Rekiichan <dtnhan.ete.dut@gmail.com>
2022-12-20 15:42:51 +0700

    update 2012

--- Program.cs
+++ Program.cs
@@ -3,6 +3,13 @@ using Amazon.DynamoDBv2;
 
 var builder = WebApplication.CreateBuilder(args);
 
+//builder.Host.ConfigureAppConfiguration((hostingContext, config) =>
+//{
+//var env = hostingContext.HostingEnvironment;
+//config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+//.AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true);
+//});
+
 // TODO: Uncomment if u want to see log in txt
 
 //Log.Logger = new LoggerConfiguration().MinimumLevel.Debug()
@@ -14,14 +21,26 @@ builder.Services.AddControllers();
 //builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
 //    builder.Configuration.GetConnectionString("DefaultConnection"))
 //);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 var awsOptions = builder.Configuration.GetAWSOptions();
 builder.Services.AddDefaultAWSOptions(awsOptions);
 builder.Services.AddAWSService<IAmazonDynamoDB>();
 builder.Services.AddScoped<IDynamoDBContext, DynamoDBContext>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
+//IConfigurationRoot configurationRoot = new ConfigurationBuilder()
+//    .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+//    .AddJsonFile("appsettings.json")
+//    .Build();
+//Config.Init(configurationRoot);
+//builder.Services.AddCors(options =>
+//{
+//    options.AddDefaultPolicy(builder =>
+//    {
+//        builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+//    });
+//});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
