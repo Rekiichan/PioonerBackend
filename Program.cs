8a1bdb45937c7008f69126836b3ff46e5c95ae4e
Rekiichan <dtnhan.ete.dut@gmail.com>
2022-12-21 21:33:34 +0700

    add identify user and authen

--- Program.cs
+++ Program.cs
@@ -1,32 +1,52 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 using Pioneer_Backend.DataAccess;
 using Pioneer_Backend.Service;
+using System.Text;
 
-var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers()
     .AddJsonOptions(
         options => options.JsonSerializerOptions.PropertyNamingPolicy = null);
 
-builder.Services.Configure<PioneerDatabaseSettings>(
+builder.Services.AddAuthentication(option =>
+{
+    option.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    option.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(x => 
+{
+    x.RequireHttpsMetadata = false;
+    x.SaveToken = true;
+    x.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(builder.Configuration.GetSection("JwtKey").ToString())),
+        ValidateIssuer = false,
+        ValidateAudience = false
+    };
+});
+
+builder.Services.Configure<DatabaseSettings>(
     builder.Configuration.GetSection("PioneerDatabase"));
 
+builder.Services.AddScoped<UserService>();
 builder.Services.AddSingleton<MemberService>();
 builder.Services.AddSingleton<RewardService>();
 builder.Services.AddSingleton<ProjectService>();
 builder.Services.AddSingleton<EventService>();
 
-
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
 //services cors
-builder.Services.AddCors(p => p.AddPolicy("corsapp", builder =>
+builder.Services.AddCors(p => p.AddPolicy("AllowAllHeadersPolicy", builder =>
 {
     builder.WithOrigins("*").AllowAnyMethod().AllowAnyHeader();
 }));
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
+app.UseCors("AllowAllHeadersPolicy");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -36,7 +56,7 @@ if (app.Environment.IsDevelopment())
 }
 app.UseCors("corsapp");
 app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
